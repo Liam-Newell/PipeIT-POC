d2c04b655436c5cfe67c65eebe5f0340afd807e3
Newell, Liam <liam_newell@manulife.com>
2019-04-29 13:03:42 -0400

    assemble also compiles!

--- build/build-assemble.js
+++ build/build-assemble.js
@@ -1,1 +1,18 @@
-module.exports = body => {};
+module.exports = body => {
+  return {
+    platform: body.platform || 'linux',
+
+    image_resource: {
+      type: body.type || '',
+      source:
+        ' # NOTE: It is important that this Docker image contains the cf command line utility as it is used by the task # which the Enterprise Pipeline intercepts and will be the context in which the deploy script will run',
+      repository: body.gitRepo
+    },
+
+    inputs: { name: 'source' },
+
+    outputs: { name: 'target' },
+
+    run: { path: 'source/concourse/scripts/assemble.sh' }
+  };
+};
