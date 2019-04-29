f1b4de8695c7bd9309b122a20f176cf98c3587a6
Newell, Liam <liam_newell@manulife.com>
2019-04-29 12:43:32 -0400

    it works!

--- compile/index.js
+++ compile/index.js
@@ -1,9 +1,15 @@
 const yamlParser = require('js-yaml');
+const fs = require('fs');
 module.exports = (config, assemble, pipeline) => {
   try {
     const configFile = yamlParser.dump(config, 'utf-8');
     const assemFile = yamlParser.dump(assemble, 'utf-8');
     const pipeFile = yamlParser.dump(pipeline, 'utf-8');
+
+    fs.writeFileSync('config.yml', configFile);
+    fs.writeFileSync('assemble.yml', assemFile);
+    fs.writeFileSync('pipeline.yml', pipeFile);
+
     return [configFile, assemFile, pipeFile];
     console.log('yammmmmmling', getYaml);
   } catch (err) {
