da6f0da354b6a29eee79efa95c4817ac2a55a3af
Newell, Liam <liam_newell@manulife.com>
2019-04-29 13:53:02 -0400

    minor clean up

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 const gut = require('./gut');
-const yamlParser = require('js-yaml');
+// const yamlParser = require('js-yaml');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,10 +19,6 @@ app.post('/form-submit', async (req, res) => {
   console.log(req.body);
   const files = gut(req.body);
   res.download(__dirname + '/concourse.zip');
-  // res.download(__dirname + '/pipeline.yml', 'pipeline.yml');
-  // res.download(__dirname + '/assemble.yml', 'assemble.yml');
-  // res.json(req.body);
-  // res.end();
 });
 
 app.listen(port, () => {
