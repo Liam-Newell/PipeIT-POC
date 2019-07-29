f839f33046206547963598bc1acf664fe4e21753
Newell, Liam <liam_newell@manulife.com>
2019-07-29 15:52:25 -0400

    now builds app based off name

--- build/build-push.js
+++ build/build-push.js
@@ -4,7 +4,7 @@ module.exports = body => {
     set -o xtrace
     cd ../../../target
     cf push -f manifest-dev.yml --no-start -------- we need the manifest created already I think
-    cf set-env app-demo MY_ENV_VAR $MY_ENV_VAR
-    cf start app-demo`;
+    cf set-env ${body.appName} MY_ENV_VAR $MY_ENV_VAR
+    cf start ${body.appName}`;
   return pushDemo;
 };
--- fly.sh
+++
@@ -1,4 +0,0 @@
-      #!/bin/bash
-    fly --target ladf login --team-name ladf --concourse-url https://concourse.platform.manulife.io --insecure
-    fly -t ladf set-pipeline -p pipeline-demo -c pipeline.yml -l config.yml
-    fly -t ladf unpause-pipeline -p pipeline-demo
